48899464eba9cdcf5723c8066d5523e14c4e5383
pankaj2525 <isarkapankaj@gmail.com>
2017-01-04 12:36:45 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,5 @@ import PackageDescription
 
 let package = Package(
   name:  "swiftysockets",
-  dependencies: [
-    .Package(url:  "https://github.com/iachievedit/Tide", majorVersion: 0),
-  ]
+  dependencies: []
 )
